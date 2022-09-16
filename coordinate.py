372f5b3899ffb1dafb5cc0da545d83183effa8db
Izael <jv.izael@hotmail.com>
2022-09-16 10:38:40 -0300

    Initial commit.

---
+++ coordinate.py
@@ -0,0 +1,13 @@
+from pair import Pair
+
+class Coordinate(Pair):
+    def __init__(self, x:int, y:int) -> None:
+        super().__init__(x, y)
+
+    def get_x(self) -> int:
+        return self.x
+
+    def get_y(self) -> int:
+        return self.y
+    
+
