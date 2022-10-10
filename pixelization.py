5505f7509c8084eb449f91f58ec2e7f3a3d87787
Izael <jv.izael@hotmail.com>
2022-10-10 15:48:12 -0300

    feat: add generate_matrix() and draw()
    The first method loads the intermediary file and generates a 3D matrix in RAM. The draw() method, for now, only prints the value each cell will have on Pixelization approach. It is not complete yet.

--- pixelization.py
+++ pixelization.py
@@ -1,40 +1,46 @@
 from curve import Curve
+from snake_curve import SnakeCurve
+from dimension import Dimension
 from coordinate import Coordinate
 import pandas as pd
+import numpy as np
 import math
 from pandas import DataFrame
 
 class Pixelization:
     def __init__(self, path:str, curve:Curve) -> None:
         self.path = path
-        file = DataFrame(pd.read_csv(path))
-        self.max_i:int = int(pd.to_numeric(file.columns[0]))
-        self.max_j:int = int(pd.to_numeric(file.columns[1]))
-        self.num_of_models:int = int(pd.to_numeric(file.columns[2]))
+        self.file = DataFrame(pd.read_csv(path))
+        self.max_i:int = int(pd.to_numeric(self.file.columns[0]))
+        self.max_j:int = int(pd.to_numeric(self.file.columns[1]))
+        self.num_of_models:int = int(pd.to_numeric(self.file.columns[2]))
         self.curve:Curve = curve
     
-    # Getters
-    def get_max_i(self) -> int:
-        return self.max_i
+    def read_to_list(self) -> list:
+        try:
+            column = DataFrame(self.file[self.file.columns[0]]).squeeze().tolist()[1:]
+            return [float(item) for item in column]
+        except:
+            raise Exception('Something went wrong when trying to read the file.')
 
-    def get_max_j(self) -> int:
-        return self.max_j
+    def generate_matrix(self):
+        #NOTE: Each matrix inside needs to be squared
+        return np.array(self.read_to_list()).reshape(self.num_of_models, 9, 3)
 
-    def get_num_of_models(self) -> int:
-        return self.num_of_models
-
-    def get_curve(self) -> Curve:
-        return self.curve
-
-    def get_number_of_elements(self) -> int:
-        return self.max_i * self.max_j * self.num_of_models
-
+    #TEST: This method is not complete. Test purposes only.
     def draw(self) -> None:
-        subcell:int = math.ceil(math.sqrt(self.num_of_models))
-        for i in range(1, self.max_i):
-            for j in range(1, self.max_j):
-                for x_axis in range(1, subcell):
-                    for y_axis in range(1, subcell):
-                        print(self.curve.get_d(Coordinate(x_axis, y_axis)))
+        matrix = self.generate_matrix()
+        for i in range(self.max_i):
+            for j in range(self.max_j):
+                for m in range(self.num_of_models):
+                    valor = matrix[m][i][j]
+                    print(valor, " ")
+                print("\n")
+            print("\n")
 
+if __name__=="__main__":
+    pixelization = Pixelization("intermediary_file.csv", SnakeCurve(27, Dimension(9, 3)))
+    # print(pixelization.draw())
+    print(pixelization.generate_matrix())
+    pixelization.draw()
 
