42b7d17b05317742ca420f5a49b32b98a07f12fa
Izael <jv.izael@hotmail.com>
2022-11-18 14:38:58 -0300

    docs: add docstring to methods on pixelization

--- webviz_plugin_boilerplate/plugins/RV/pixelization.py
+++ webviz_plugin_boilerplate/plugins/RV/pixelization.py
@@ -17,18 +17,44 @@ class Pixelization:
         self.num_of_models:int = int(pd.to_numeric(self.file.columns[2]))
     
     def read_to_list(self) -> list:
+        """
+        Read a column into a list data structure.
+
+        Iterates through the first column in the .csv file and converts it into a list.
+
+        Return:
+            It return a list with numpy floats of 16 bytes.
+            
+        """
+
         try:
             column = DataFrame(self.file[self.file.columns[0]]).squeeze().tolist()
-            return [np.float64(item) for item in column]
+            return [np.float16(item) for item in column]
         except:
             raise Exception('Something went wrong when trying to read the file.')
 
-    # Separate each model in a matrix for itself
     def generate_model_matrix(self):
+        """
         #NOTE: Each matrix inside needs to be squared
+        Separates the list into sublists of the same model.
+
+        Returns:
+            Numpy array of shape (num_of_models, max_i, max_j)
+        """
         return np.array(self.read_to_list(), dtype=np.float64).reshape(self.num_of_models, self.max_i, self.max_j)
 
     def draw(self):
+        """
+        Draws the pixelization based on the model matrix.
+
+        It reorganizes data into an array following the rules of the Pixelization Visualization technique.
+        First, we need to be sure we are dealing with swuared models. If it is not squared, we need to be sure we have the
+        same amount of NaN values at the beginning and at the end of the array. After calculating those values we iterate
+        through the array, stacking the elements based on the depth (models).
+
+        Returns:
+            Numpy array with the elements reorganized.
+        """
         shape:int = math.ceil(math.sqrt(self.num_of_models))
         prev = math.ceil(((shape ** 2) - self.num_of_models) / 2)
         next = math.floor(((shape ** 2) - self.num_of_models) / 2)
@@ -47,6 +73,12 @@ class Pixelization:
         return np.array(result)
 
     def pad_matrix(self):
+        """
+        It adds NaN value around each submatrix to give a space between each one, making it easier to visualize and distinct the submatrices.
+
+        Returns:
+            Numpy array with NaN elements on the edges.
+        """
         matrix = self.draw()
         padded_array = []
         for element in matrix:
@@ -55,15 +87,16 @@ class Pixelization:
         return np.array(padded_array)
 
 
-    def generate_csv_file(self) -> None:
+    def generate_image(self) -> None:
+        """
+        It generates and save the image based on the matrix (multidimensional array) received.
+
+        Returns:
+            None.
+        """
         array = self.pad_matrix()
         array = np.array(np.dstack(np.array_split(array, self.max_i)))
         array = array.reshape(array.shape[0] * array.shape[1], array.shape[2])
-        print(f'Shape of the array: {array.shape}')
-        print("Original array")
-        print(array)
-        print("Inverted array")
-        print(array[::-1])
 
         try:
             plt.figure(figsize=(self.max_j, self.max_i), layout="constrained")
@@ -74,6 +107,6 @@ class Pixelization:
 
 
 if __name__=="__main__":
-    pixelization = Pixelization("/home/izael/git/rvweb-python/tests_files/intermediary_file.csv")
-    pixelization.generate_csv_file()
+    pixelization = Pixelization("/home/izael/git/rvweb-python/tests_files/arithmetic-mean.csv")
+    pixelization.generate_image()
 
