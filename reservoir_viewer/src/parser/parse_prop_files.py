88c73d1ebebe933b3a2742490ab54974a205a801
Izael Souza <jv.izael@hotmail.com>
2023-09-21 01:51:50 -0300

    fix: parser file generator

--- reservoir_viewer/src/parser/parse_prop_files.py
+++ reservoir_viewer/src/parser/parse_prop_files.py
@@ -1,6 +1,17 @@
 import pandas as pd
+import csv
 
 class ParseProperties:
     def parse_file(self, save, directory, properties) -> None:
-        file = pd.read_csv(directory, sep=";", usecols=[properties])
-        file.to_csv(save)
+        file = pd.read_csv(directory, sep=";", usecols=["I", "J", "Model", properties]).fillna("nan")
+
+        i_max = file['I'].max()
+        j_max = file['J'].max()
+        model_max = file['Model'].max()
+        values = str('{},{},{},{}'.format(i_max, j_max, model_max, properties))
+
+        new_record = pd.DataFrame([values])
+        final = file[properties]
+        final = pd.concat([new_record, final], ignore_index=True)
+
+        final.to_csv(save, index=False, header=False, doublequote=False, quoting=csv.QUOTE_NONE, quotechar="", escapechar=" ")
