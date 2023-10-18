c066d8b310b36f604c1cdd815268f5019b1e2c6c
Izael Souza <jv.izael@hotmail.com>
2023-10-18 02:05:52 -0300

    refactor: remove unnecessary code

--- reservoir_viewer/src/small_multiples.py
+++ reservoir_viewer/src/small_multiples.py
@@ -46,37 +46,6 @@ class SmallMultiples:
             self.num_of_models, self.max_i, self.max_j
         )
 
-    def mergeSort(self, alist, cluster_dict):
-        if len(alist) > 1:
-            mid = len(alist) // 2
-            left_half = alist[:mid]
-            right_half = alist[mid:]
-
-            self.mergeSort(left_half, cluster_dict)
-            self.mergeSort(right_half, cluster_dict)
-
-            i = 0
-            j = 0
-            k = 0
-            while i < len(left_half) and j < len(right_half):
-                if cluster_dict[left_half[i]] < cluster_dict[right_half[j]]:
-                    alist[k] = left_half[i]
-                    i = i + 1
-                else:
-                    alist[k] = right_half[j]
-                    j = j + 1
-                k = k + 1
-
-            while i < len(left_half):
-                alist[k] = left_half[i]
-                i = i + 1
-                k = k + 1
-
-            while j < len(right_half):
-                alist[k] = right_half[j]
-                j = j + 1
-                k = k + 1
-
     def get_clusters(self, matrix, iterations, max_clusters):
         xmeans_instance = XmeansClusterization(matrix, iterations, max_clusters)
         return xmeans_instance.cluster_models()
